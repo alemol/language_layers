7a42c392cbc0b14e90535d3f66b4aade7c04c40d
Alejandro Molina Villegas <amolina@centrogeo.edu.mx>
2020-08-01 11:36:05 -0500

    grid changes

--- affix_gridsearch.py
+++ affix_gridsearch.py
@@ -15,13 +15,10 @@ if __name__ == '__main__':
 
     lang = 'es'
     affixes = get_afixes(lang)
-    vocab_sizes = [i for i in range(258,2**13, 50)]
-    subword_lengths = [3,4,5,6,7,8,9,10,11,12]
-    #subword_lengths = [3,5,7,9,11]
-    #subword_lengths = [5]
-    grid = []
-    for vocab_size in vocab_sizes:
-        for subword_len in subword_lengths:
+    vocab_sizes = [i for i in range(300,500,100)]
+    subword_lengths = [3,5,7,9,11]
+    for subword_len in subword_lengths:
+        for vocab_size in vocab_sizes:
             # Subword encoder build
             encoder = tfds.features.text.SubwordTextEncoder.build_from_corpus(
              corpus_generator=text_generator(DATA_DIR),
@@ -39,24 +36,6 @@ if __name__ == '__main__':
             entdist = pd.Series(lexcoder.ents_subwords)
             del(lexcoder)
             # create grid entry
-            outcome = {
-                'lan': lang,
-                'vocab_size': vocab_size,
-                'subword_len': subword_len,
-                'affix_coverage': affix_coverage,
-                'emin': entdist.min(),
-                'emean': entdist.mean(),
-                'estd': entdist.std(),
-                'emax': entdist.max(),
-                'ekurt': entdist.kurtosis(),
-                'eskew': entdist.skew()
-            }
+            outcome = '{},{},{},{},{},{},{},{},{},{}'.format(lang,vocab_size,subword_len,affix_coverage,entdist.kurtosis(),entdist.skew(),entdist.min(),entdist.mean(),entdist.std(),entdist.max())
             print(outcome)
             del(entdist)
-            grid.append(outcome)
-
-    grid_df = pd.DataFrame(grid)
-    grid_df.to_csv('grid_subwords_'+lang+'.csv', index = False)
-
-    df_top = grid_df.sort_values(["affix_coverage"], ascending = (False))
-    print(df_top[["vocab_size","affix_coverage"]][0:20])
